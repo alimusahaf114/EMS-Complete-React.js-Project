a3c36b1409f0f40314b65c7906ffb4a72d059702
alimusahaf114 <musahafali43@gmail.com>
2025-07-16 09:54:22 +0500

    Added different pages code

--- EMS/src/App.jsx
+++ EMS/src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Login from "./component/Auth/login";
 import EmployeeDashboard from "./component/Dashboard/EmployeeDashboard";
+import AdminDashboard from "./component/Dashboard/AdminDashboard";
 
 const App = () => {
   return (
     <div>
       {/* <Login /> */}
-      <EmployeeDashboard />
+      {/* <EmployeeDashboard /> */}
+      <AdminDashboard />
     </div>
   );
 };
